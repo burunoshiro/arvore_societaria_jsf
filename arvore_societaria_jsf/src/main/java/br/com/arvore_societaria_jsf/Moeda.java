02f37d5cfb90f781b3b7ba282d9876c7f8672dca
Bruno <Bruno@Bruno-Tablet>
2015-06-30 21:05:00 -0300

    Classe para teste fde hibernate inserida

---
+++ arvore_societaria_jsf/src/main/java/br/com/arvore_societaria_jsf/Moeda.java
@@ -0,0 +1,35 @@
+package br.com.arvore_societaria_jsf;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Moeda {
+
+	@GeneratedValue @Id
+	private Integer id;
+	private String nome;
+	private String pais;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getPais() {
+		return pais;
+	}
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+	
+	
+	
+}
