2d0a7296fd3af37047aec98a179ae9366e655c07
Bruno <burunoshiro@gmail.com>
2016-04-08 16:08:50 -0300

    Erro na página de busca de moeda consertado.

--- arvore_societaria_jsf/src/main/java/br/com/arvore_societaria_jsf/managedbean/EmpresaBean.java
+++ arvore_societaria_jsf/src/main/java/br/com/arvore_societaria_jsf/managedbean/EmpresaBean.java
@@ -6,6 +6,7 @@ import javax.faces.bean.SessionScoped;
 import br.com.arvore_societaria_jsf.bean.Empresa;
 
 @ManagedBean
+@SessionScoped
 public class EmpresaBean {
 
 	Empresa empresa = new Empresa();
